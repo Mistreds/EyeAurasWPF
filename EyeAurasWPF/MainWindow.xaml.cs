ec04126f6661beb0b69326ca62b07f9e3334e111
Mistred <spiridonov24@gmail.com>
2021-11-10 11:35:35 +0300

    1

--- EyeAurasWPF/MainWindow.xaml.cs
+++ EyeAurasWPF/MainWindow.xaml.cs
@@ -24,15 +24,17 @@ namespace EyeAurasWPF
     {
         private class JsonCreate
         {
-            public bool logpass { get; set; }
-            public string path { get; set; }
-            public string arg { get; set; }
-            public string Login { get; set; }
-            public string Password { get; set; }
-            public bool Telegram { get; set; }
-            public string token { get; set; }
-            public string chatid { get; set; }
-            public JsonCreate(bool logpass, string path, string arg, string login, string password, bool telegram, string token, string chatid)
+            public string host { get; private set; }
+            public bool logpass { get; private set; }
+            public string path { get; private set; }
+            public string arg { get; private set; }
+            public string Login { get; private set; }
+            public string Password { get; private set; }
+            public bool Telegram { get; private set; }
+            public string token { get; private set; }
+            public string chatid { get; private set; }
+            
+            public JsonCreate(bool logpass, string path, string arg, string login, string password, bool telegram, string token, string chatid,string host)
             {
                 this.logpass = !logpass;
                 if(logpass==true)
@@ -50,13 +52,18 @@ namespace EyeAurasWPF
                 Telegram = telegram;
                 this.token = token;
                 this.chatid = chatid;
+                this.host = host;
             }
             public async Task CreateJson()
             {
                 try
                 {
+                    var options = new JsonSerializerOptions
+                    {
+                        WriteIndented = true
+                    };
 
-                
+
                     var path_json = Environment.ExpandEnvironmentVariables("%appdata%/EyeAuras/EyeSquad/");
                     if (!Directory.Exists(path_json)) Directory.CreateDirectory(path_json);
                     path_json = $"{path_json}//id.config";
@@ -65,7 +72,7 @@ namespace EyeAurasWPF
                     using (FileStream fs = new FileStream($"{path_json}", FileMode.OpenOrCreate))
                 {
                     
-                    await JsonSerializer.SerializeAsync<JsonCreate>(fs, this);
+                    await JsonSerializer.SerializeAsync<JsonCreate>(fs, this, options);
                     
                 }
                 }
@@ -82,8 +89,9 @@ namespace EyeAurasWPF
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            JsonCreate json = new JsonCreate((bool)Login_Arg_toogle.IsChecked, Path.Text, Arg.Text, Login.Text, Password.Text, (bool)telegram.IsChecked, Token.Text,Chatid.Text) ;
+            JsonCreate json = new JsonCreate((bool)Login_Arg_toogle.IsChecked, Path.Text, Arg.Text, Login.Text, Password.Text, (bool)telegram.IsChecked, Token.Text,Chatid.Text, Host.Text) ;
             _=json.CreateJson();
+            Close();
         }
     }
 }
